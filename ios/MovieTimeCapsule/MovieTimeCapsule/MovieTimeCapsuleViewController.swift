a3a5ea585c69d5bf2567749cb939045b55e9317c
xiaolizi0v0 <1595096901@qq.com>
2026-09-02 12:18:38 +0800

    Add iPad GitHub Actions packaging

---
+++ ios/MovieTimeCapsule/MovieTimeCapsule/MovieTimeCapsuleViewController.swift
@@ -0,0 +1,63 @@
+import UIKit
+import WebKit
+
+final class MovieTimeCapsuleViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    private var webView: WKWebView!
+
+    override func loadView() {
+        let configuration = WKWebViewConfiguration()
+        configuration.allowsInlineMediaPlayback = true
+
+        if #available(iOS 10.0, *) {
+            configuration.mediaTypesRequiringUserActionForPlayback = []
+        }
+
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        webView.allowsBackForwardNavigationGestures = false
+
+        self.webView = webView
+        self.view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        loadBundledPage()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+
+    private func loadBundledPage() {
+        guard let bundleURL = Bundle.main.resourceURL else { return }
+
+        let pageURL = bundleURL.appendingPathComponent("Resources/tools/MovieTimeCapsule.html")
+        guard FileManager.default.fileExists(atPath: pageURL.path) else { return }
+
+        webView.loadFileURL(pageURL, allowingReadAccessTo: bundleURL)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        createWebViewWith configuration: WKWebViewConfiguration,
+        for navigationAction: WKNavigationAction,
+        windowFeatures: WKWindowFeatures
+    ) -> WKWebView? {
+        if let url = navigationAction.request.url {
+            UIApplication.shared.open(url)
+        }
+        return nil
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        decisionHandler(.allow)
+    }
+}
